9dd4550635664d09982f795ba30903f532610b09
sylvainthemonk <sylvain.lem1@gmail.com>
2014-06-21 18:24:24 +0200

    mise en place premier dao

--- mhanalyzer-backend/src/main/java/com/elmonko/dao/MonsterDao.java
+++ mhanalyzer-backend/src/main/java/com/elmonko/dao/MonsterDao.java
@@ -1,9 +1,38 @@
 package com.elmonko.dao;
 
+import com.elmonko.model.Monster;
+
 /**
  * Created by dev on 19/06/2014.
  */
 public interface MonsterDao {
+
+    /**
+     * Create a new monster
+     * @param monster Monster to create
+     * @return created monster
+     */
+    Monster create(Monster monster);
+
+    /**
+     * Update a monster
+     * @param monster Monster to update
+     * @return up-to-date monster
+     */
+    Monster update(Monster monster);
+
+    /**
+     * Delete a monster
+     * @param monster Monster to delete
+     */
+    void delete(Monster monster);
+
+    /**
+     * Find a monster by id
+     * @param id id of the monster
+     * @return result
+     */
+    Monster find(long id);
 
     
 }
--- mhanalyzer-backend/src/main/java/com/elmonko/dao/MonsterDaoImpl.java
+++ mhanalyzer-backend/src/main/java/com/elmonko/dao/MonsterDaoImpl.java
@@ -1,7 +1,29 @@
 package com.elmonko.dao;
 
+import com.elmonko.model.Monster;
+
 /**
  * Created by ElMonko on 19/06/2014.
  */
 public class MonsterDaoImpl implements MonsterDao {
+
+    @Override
+    public Monster create(Monster monster) {
+        return null;
+    }
+
+    @Override
+    public Monster update(Monster monster) {
+        return null;
+    }
+
+    @Override
+    public void delete(Monster monster) {
+
+    }
+
+    @Override
+    public Monster find(long id) {
+        return null;
+    }
 }
